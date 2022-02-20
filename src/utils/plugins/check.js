0eb97bf542e7c9c23c8b4e975dfa50641559cd6d
sommio <sommio.mail@gmail.com>
2022-02-21 00:54:10 +0800

    update

--- src/utils/plugins/check.js
+++ src/utils/plugins/check.js
@@ -1,7 +1,7 @@
 const check = {
   mail (m) {
     return {
-      k: /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(m),
+      k: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(m),
       v: m
     }
   },
